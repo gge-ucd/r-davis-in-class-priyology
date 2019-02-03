d22f40dc4c981504bfe07809f1c446dbf2edcccd
unknown <prishukla.sci@gmail.com>
2019-02-03 14:39:32 -0800

    finally able to generate a histogram!

--- script/week_4_assignment_ABC.R
+++ script/week_4_assignment_ABC.R
@@ -5,3 +5,8 @@ surveys <- read.csv("data/portal_data_joined.csv")
 ## Subset to just the first column and columns five through 8. Include only the first 400 rows. Save this as a dataframe called  surveys_subset.
 surveys_subset <- surveys[1:400,c(1, 5:8)]
 surveys_subset
+
+## CHALLENGE: Select all rows that have a hindfoot_length greater than 32, save these in a new data.frame named  surveys_long_feet, then plot its hindfoot_length values as a histogram using the hist function.
+surveys_long_feet <- surveys_subset[surveys_subset$hindfoot_length > 32, ]
+surveys_long_feet
+hist(surveys_long_feet$hindfootlength)
