2e7c2a74e5680cb1ccbf9460ed875f06505c6387
unknown <prishukla.sci@gmail.com>
2019-02-03 17:10:04 -0800

    I tried to create a histogram of hindfoot_lenghts and it doesn't work!
    Hmmm ......

--- script/week_4_assignment_ABC.R
+++ script/week_4_assignment_ABC.R
@@ -9,4 +9,10 @@ surveys_subset
 ## CHALLENGE: Select all rows that have a hindfoot_length greater than 32, save these in a new data.frame named  surveys_long_feet, then plot its hindfoot_length values as a histogram using the hist function.
 surveys_long_feet <- surveys_subset[surveys_subset$hindfoot_length > 32, ]
 surveys_long_feet
-hist(surveys_long_feet$hindfootlength)
+complete.cases(surveys_long_feet)
+surveys_long_feet <- na.omit(surveys_long_feet)
+surveys_long_feet
+hist(surveys_long_feet$hindfoot_length)
+hindfoot_lengths <- as.character(surveys_long_feet$hindfoot_length)
+hindfoot_lengths
+hist(hindfoot_lengths)
