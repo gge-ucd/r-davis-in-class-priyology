4ca9a59ed81c505bc9072e318c6a1838ee4b3fa0
unknown <prishukla.sci@gmail.com>
2019-02-13 10:27:21 -0800

    at 41:17 in class
    ggplot rules

--- script/week_6_class_code.R
+++ script/week_6_class_code.R
@@ -1,3 +1,50 @@
 #Week 6 Live Code
 
-Definitely ready
+library(tidyverse)
+
+###### finishing up dplyr ========================
+surveys <- read_csv("data/portal_data_joined.csv")
+
+#take out all NAs from weight, hindfoot_length, and sex
+
+surveys_complete <- surveys %>%
+  filter(!is.na(weight)) %>% 
+  filter(!is.na(hindfoot_length)) %>% 
+  filter(!is.na(sex)) 
+
+surveys_complete <- surveys %>%
+  filter(!is.na(weight), !is.na(hindfoot_length), !is.na(sex)) 
+  
+species_counts <- surveys_complete %>% 
+  group_by(species_id) %>% 
+  tally() %>% 
+  filter(n >= 50)
+
+surveys_complete <- surveys_complete %>% 
+  filter(species_id %in% species_counts$species_id)
+
+species_keep <- c("DM", "DO") #example of list we could use instead of the row in a dataframe
+
+#writing your dataframe to .csv
+
+write_csv(surveys_complete, path = "data_output/surveys_complete.csv")
+
+
+# Wed Feb 13 10:16:08 2019 ------------------------------
+## ^ made via 'ts + TAB'
+
+####ggplot time! ======================
+library(ggplot2)
+
+#ggplot(data = DATA, mapping = aes(MAPPINGS)) + 
+#geom_function()
+
+ggplot(data = surveys_complete)
+
+#define a mapping 
+
+ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length)) +
+  geom_point()
+
+
+
